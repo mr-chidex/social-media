dfa8d6aaab13469b5cb0c32f1d3188704cd8baf1
mr-chidex <mbahchiboy2@gmail.com>
2022-06-02 12:40:13 +0100

    connect db

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,27 @@
-import express from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
 
-const app = express();
+const app: Application = express();
+dotenv.config();
+const apiVersion = process.env.API_VERSION || "v1";
 
-app.listen(3000, () => console.log("app running on Port 3000"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
+
+// error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (process.env.NODE_ENV === "development") {
+    return res
+      .status(500)
+      .json({ message: err.message, error: true, stack: err.stack });
+  }
+  res.status(500).json({ message: err.message, error: true });
+});
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+import app from "./app";
+const PORT = process.env.PORT || 5000;
+
+//connect db and start server
+mongoose
+  .connect(process.env.DATABASE_URL!)
+  .then(() => {
+    console.log("db connected");
+    app.listen(PORT, () => console.log(`server running at PORT - ${PORT}`));
+  })
+  .catch((err) => console.error(err));
